cc2d6eb0ce62363bd0da3bf63ba10d97dfa93e98
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-04 11:21:47 +0900

    Maaping Test

--- src/main/java/com/vroong/bootcamp/HelloController.java
+++ src/main/java/com/vroong/bootcamp/HelloController.java
@@ -1,12 +1,16 @@
 package com.vroong.bootcamp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello, Spring";
+
+    @PostMapping("/customers")
+    public Customer customer(@RequestBody Customer dto){
+        dto.setAge(dto.getAge() + 1);
+        return dto;
     }
 }

80ab9877f5c850516cddde47a324646eb001a4b1
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-04 12:39:23 +0900

    validation check

--- src/main/java/com/vroong/bootcamp/Customer.java
+++ src/main/java/com/vroong/bootcamp/Customer.java
@@ -2,12 +2,14 @@ package com.vroong.bootcamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
 public class Customer {
+    @NonNull
     private String name;
     private int age;
 }
--- src/main/java/com/vroong/bootcamp/HelloController.java
+++ src/main/java/com/vroong/bootcamp/HelloController.java
@@ -1,15 +1,18 @@
 package com.vroong.bootcamp;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        log.info("reached");
-        return "Hello, Spring?";
+    @PostMapping("/customers")
+    public Customer customer(@RequestBody @Valid Customer dto) {
+        dto.setAge(dto.getAge() + 1);
+        return dto;
     }
 }

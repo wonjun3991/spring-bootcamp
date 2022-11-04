33f81043a3a3bed99de19a0f9dcee5280bc3e191
wonjun3991 <wonjun.choi@meshkorea.net>
2022-11-04 12:32:30 +0900

    logging

--- src/main/java/com/vroong/bootcamp/HelloController.java
+++ src/main/java/com/vroong/bootcamp/HelloController.java
@@ -1,23 +1,15 @@
 package com.vroong.bootcamp;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
-    CustomProperties customProperties;
-
-    @Value("${foo.bar}")
-    String value;
-
-    @GetMapping("/properties")
-    public String properties() {
-        return value;
-    }
-
-    @GetMapping("/properties2")
-    public CustomProperties properties2() {
-        return customProperties;
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("reached");
+        return "Hello, Spring?";
     }
 }
